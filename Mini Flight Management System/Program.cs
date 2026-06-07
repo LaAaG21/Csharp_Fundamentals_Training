1d868fb7739b0ecfac6d9e6d2b0ed037dd76e79c
Al Aiham <ayhem9987@gmail.com>
2026-06-07 09:30:07 +0400

    Added Menu

--- Mini Flight Management System/Program.cs
+++ Mini Flight Management System/Program.cs
@@ -23,6 +23,34 @@
         static Dictionary<string,string> passengerSeatMap = new Dictionary<string,string>();
 
 
+        public static string menu()
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 .   Register New Passenger");
+            Console.WriteLine("2 .   View All Passengers");
+            Console.WriteLine("3 .   Book a Flight Ticktes");
+            Console.WriteLine("4 .   View Booking Details");
+            Console.WriteLine("5 .   Update a Booking");
+            Console.WriteLine("6 .   Cancel a Ticket");
+            Console.WriteLine("7 .   Passenger Check-In");
+            Console.WriteLine("8 .   Board Passengers (Bording Stack)");
+            Console.WriteLine("9 .   Generate Flight Manifeet");
+            Console.WriteLine("10.   Manage Waitlist & Seat Assignment");
+            Console.WriteLine("0 .   Exit");
+
+            Console.WriteLine();
+
+            Console.WriteLine("===========================================");
+            Console.Write("Enter Your Choice: ");
+
+            return Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             
