e28b1e95e79573d778eb812a78b91e61ddb92659
Al Aiham <ayhem9987@gmail.com>
2026-06-07 09:50:06 +0400

    Enhanced The UI

--- Mini Flight Management System/Program.cs
+++ Mini Flight Management System/Program.cs
@@ -25,26 +25,35 @@
 
         public static string menu()
         {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("===========================================");
             Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
             Console.WriteLine("===========================================");
 
             Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("1 .   Register New Passenger");
             Console.WriteLine("2 .   View All Passengers");
             Console.WriteLine("3 .   Book a Flight Ticktes");
             Console.WriteLine("4 .   View Booking Details");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("5 .   Update a Booking");
             Console.WriteLine("6 .   Cancel a Ticket");
             Console.WriteLine("7 .   Passenger Check-In");
             Console.WriteLine("8 .   Board Passengers (Bording Stack)");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("9 .   Generate Flight Manifeet");
             Console.WriteLine("10.   Manage Waitlist & Seat Assignment");
+
+            Console.ResetColor();
             Console.WriteLine("0 .   Exit");
 
             Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("===========================================");
             Console.Write("Enter Your Choice: ");
 
@@ -65,48 +74,64 @@
                 {
 
                     case "0":
+                        Console.ResetColor();
                         Console.WriteLine("GoodBey..");
                         lop = false;
                         break;
 
                     case "1":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "2":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "3":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "4":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "5":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "6":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "7":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "8":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "9":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
                         break;
 
                     case "10":
+                        Console.ResetColor();
                         Console.WriteLine("Under Devalopment...");
+                        break;
+
+                    default:
+                        Console.ResetColor();
+                        Console.WriteLine("Invalid Option.. Please Select From 0 to 10");
                         break;
 
                 }
