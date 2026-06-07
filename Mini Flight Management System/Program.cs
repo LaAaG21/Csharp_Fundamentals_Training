79ad1649b4e4e06f21a9d011006a2f3bbfa24c19
Al Aiham <ayhem9987@gmail.com>
2026-06-07 09:21:02 +0400

    Added New Project

---
+++ Mini Flight Management System/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Mini_Flight_Management_System
+{
+    internal class Program
+    {
+        // List Variables
+        static List<string> passengerNames = [];
+        static List<string> ticketNumbers = [];
+        static List<string> availableDates = [];
+        static List<string> cancelledTickets = [];
+
+        // Array Variables
+        static string[] flightNumbers = { "OA101", "OA102", "OA103", "OA104", "OA105", "OA106" };
+        
+        // Queue Variables
+        static Queue<string> checkedInQueue = new Queue<string>();
+        static Queue<string> waitlistQueue = new Queue<string>();
+
+        // Stack Variables
+        static Stack<string> boardingStack = new Stack<string>();
+
+        // Dictionary Variables
+        static Dictionary<string, string> bookingRecord = new Dictionary<string, string>();
+        static Dictionary<string,string> passengerSeatMap = new Dictionary<string,string>();
+
+
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
